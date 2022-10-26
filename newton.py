38d4022e7440ab356c7718ffb16e82d0a56c8d91
mesaMason <mesaMason@users.noreply.github.com>
2022-10-25 21:55:12 -0400

    Update newton.py

--- newton.py
+++ newton.py
@@ -1,45 +1,62 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Newton:
     ''' Finds coefficients of Newton's polynomial using memoization
     '''
+
     def __init__(self, x, y):
         self._memo = {}
         self._x = x
         self._y = y
 
-    ''' Provide a tuple of (highest power, 0) will calculate and store the
-        divided differences
-    '''
-    def calculate(self, f_k2_k1):
-        assert(len(f_k2_k1) == 2)
-        if f_k2_k1 in self._memo:
-            return self._memo[f_k2_k1]
+    def calculate(self, f_k_j):
+        ''' Divided difference f[x_k, ... , x_j] is represented by the tuple (x_k, x_j)
+        '''
+        assert(len(f_k_j) == 2)
+        if f_k_j in self._memo:
+            return self._memo[f_k_j]
 
-        if f_k2_k1[0] == f_k2_k1[1]:
-            self._memo[f_k2_k1] = self._y[f_k2_k1[0]]
-            return self._memo[f_k2_k1]
+        # base case of f(x_n) = y_n, represented by the tuple (x_n, x_n)
+        if f_k_j[0] == f_k_j[1]:
+            self._memo[f_k_j] = self._y[f_k_j[0]]
+            return self._memo[f_k_j]
 
-        start = f_k2_k1[0]
-        end = f_k2_k1[1]
+        start = f_k_j[0]
+        end = f_k_j[1]
         start_minus_1 = start - 1
         end_plus_1 = end + 1
-        f_b = (start, end + 1)
-        f_a = (start - 1, end)
 
-        self._memo[f_k2_k1] = (self.calculate(f_b) - self.calculate(f_a)) / (self._x[start] - self._x[end])
-        return self._memo[f_k2_k1]
+        f_b = (start, end + 1) # f[x_k, ... , x_j+1]
+        f_a = (start - 1, end) # f[x_k-1, ... , x_j]
+
+        self._memo[f_k_j] = \
+            (self.calculate(f_b) - self.calculate(f_a)) / (self._x[start] - self._x[end])
+
+        return self._memo[f_k_j]
             
-    ''' Retrieve the coefficients 
-    '''
     def coefficients(self):
+        ''' Retrieve the coefficients 
+        '''
         coef = []
         k = len(self._x)
-        for i in range(1, k+1):
-            lookup = (k - i, 0)
+        for i in range(0, k):
+            lookup = (i, 0)
             coef.append(self._memo[lookup])
         return coef
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Regression')
+    parser.add_argument("-n",
+                        "--num_points",
+                        required=True,
+                        type=int,
+                        help='Number of test data points to generate')
+    args = parser.parse_args()
+    if args.num_points < 1:
+        raise Exception("num_points must be greater than 0")
+    return args
 
 def generate_random_points(num_points):
     x = []
@@ -56,16 +73,16 @@ def newton_polynomial(x, data_x, coefficients):
         https://pages.cs.wisc.edu/~amos/412/lecture-notes/lecture08.pdf
     '''
     assert(len(data_x) == len(coefficients))
-    n = len(coefficients)
-    p = coefficients[n-1]
+    n = len(coefficients) - 1
+    p = coefficients[n]
     for i in range(0, n):
-        j = n - 2 - i
+        j = n - i - 1
         p = coefficients[j] + (x - data_x[j]) * p
     return p
 
 def main():
-    # number of data points (also degree of polynomial + 1)
-    num_points = 4
+    args = parse_args()
+    num_points = args.num_points
 
     # generate random points around line x = y
     data_x, data_y = generate_random_points(num_points)
@@ -77,21 +94,20 @@ def main():
     coefficients = newton.coefficients()
 
     # create the range for the polynomial's plot
-    step = 0.0001
-    start = 10
-    end = start + num_points + step - 1
-    x = np.arange(start, end, step)
+    step = 0.01
+    start = 10 - 1/num_points
+    end = 10 + num_points - 1 + 1/num_points
 
-    # plot the polynomial given the coefficients
-    plt.plot(x, newton_polynomial(x, data_x, coefficients), color='r')
+    # evaluate and plot the polynomial given the coefficients
+    x = np.arange(start, end, step)
+    y = newton_polynomial(x, data_x, coefficients)
+    plt.plot(x, y, color='r')
 
     # scatter plot the generated data points
     plt.scatter(data_x, data_y, color='b')
 
     # show the plots
     plt.show()
-    
 
 if __name__ == '__main__':
     main()
-
